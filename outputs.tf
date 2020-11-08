11e052484d5bae8930bb6ddb74f080232d6d4a39
Ben Windsor <ben.windsor@skyrad.co.uk>
2020-11-08 13:23:08 +0000

    Fix domain_name output

--- outputs.tf
+++ outputs.tf
@@ -14,11 +14,11 @@ output "api_name" {
 }
 
 output "invoke_url" {
-  value = var.custom_domain == null ? aws_api_gateway_deployment.api-deployment.invoke_url : module.custom_domain.invoke_url
+  value = var.custom_domain == null ? aws_api_gateway_deployment.api-deployment.invoke_url : module.custom_domain[0].invoke_url
   description = "Full URL to use to invoke the API, for example https://api.example.com/v1"
 }
 
 output "domain_name" {
-  value = var.custom_domain == null ? substr(aws_api_gateway_deployment.api-deployment.invoke_url, 0, length(aws_api_gateway_deployment.api-deployment.invoke_url - length(var.stage_name) - 1)) : module.custom_domain.domain_name
+  value = var.custom_domain == null ? substr(aws_api_gateway_deployment.api-deployment.invoke_url, 0, length(aws_api_gateway_deployment.api-deployment.invoke_url - length(var.stage_name) - 1)) : module.custom_domain[0].domain_name
   description = "Domain name the API is hosted at, for example api.example.com"
 }
