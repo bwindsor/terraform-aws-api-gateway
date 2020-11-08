322320ecbad7d1f4f118a58a2129d2f69e4069cf
Ben Windsor <ben.windsor@skyrad.co.uk>
2020-11-08 12:02:33 +0000

    Add domain_name output

--- custom_domain/outputs.tf
+++ custom_domain/outputs.tf
@@ -1,3 +1,6 @@
 output "invoke_url" {
   value = format("https://%s/%s", aws_api_gateway_base_path_mapping.map_api_to_custom_domain.domain_name, aws_api_gateway_base_path_mapping.map_api_to_custom_domain.base_path)
 }
+output "domain_name" {
+  value = aws_api_gateway_base_path_mapping.map_api_to_custom_domain.domain_name
+}
