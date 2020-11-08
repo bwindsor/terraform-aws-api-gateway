42a77664f1d80c7264ae51009f92fba70ee7c69a
Ben Windsor <ben.windsor@skyrad.co.uk>
2020-11-08 14:42:53 +0000

    Update stage_name reference to ensure custom domain is created after gateway stage

--- custom_domain.tf
+++ custom_domain.tf
@@ -4,7 +4,7 @@ module "custom_domain" {
   source = "./custom_domain"
 
   aws_api_gateway_rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name = var.stage_name
+  stage_name = aws_api_gateway_stage.api_deployed_stage.stage_name
   base_path = var.custom_domain.base_path
   domain_name = var.custom_domain.domain_name
   hosted_zone_name = var.custom_domain.hosted_zone_name
